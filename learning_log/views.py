c9f13618ae67b705fffb787c378736997b247f03
coderobel <robel.ugr-4552-16@aau.edu.et>
2026-02-13 23:15:14 +0300

    created views which handle adding, editing and viewing topics and entries for topics

--- learning_log/views.py
+++ learning_log/views.py
@@ -1,5 +1,54 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import  HttpResponseRedirect
+from .models import Topic,Entry
+from .forms import TopicForm, EntryForm
+from django.urls import reverse
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Hi</h1>')
+    return render(request, 'learning_log/home.html')
+def topics(request):
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics' : topics}
+    return render(request, 'learning_log/topics.html', context)
+def topic(request, topic_id):
+    topic = Topic.objects.get(id = topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic' : topic, 'entries' : entries}
+    return render(request, 'learning_log/topic.html', context)
+def new_topic(request):
+    if request.method != 'POST':
+        form = TopicForm()
+    else: 
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_log:topics'))
+    context = {'form' : form}
+    return render(request, 'learning_log/new_topic.html', context)
+def new_entry(request,topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('learning_log:topic',args=[topic_id]))
+        
+    context = {'topic': topic, 'form' : form}
+    return render(request, 'learning_log/new_entry.html',context)
+def edit_entry(request, entry_id):
+    entry = Entry.objects.get(id= entry_id)
+    topic = entry.topic
+    if request.method != 'POST':
+        form = EntryForm(instance=entry)
+    else:
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_log:topic', args=[topic.id]))
+        
+    context =  {'entry' : entry, 'form' : form}
+    return render(request, 'learning_log/edit_entry.html', context)
