0c7fc5f17f4dd36a65ebcef6a99acc82f9de0ce5
coderobel <robel.ugr-4552-16@aau.edu.et>
2026-02-13 23:48:57 +0300

    creates logout functionality

--- learning_logs/settings.py
+++ learning_logs/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'learning_log.apps.LearningLogConfig',
-    'users.apps.UserConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
--- users/views.py
+++ users/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.contrib.auth import logout
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 # Create your views here.
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('learning_log:home'))
