2484f582299eefd33a6788071fd1a4ad74bb381b
coderobel <robel.ugr-4552-16@aau.edu.et>
2026-02-14 10:36:19 +0300

    update topics model to have an owner attribute and created functionality to allow users to own their data

--- users/views.py
+++ users/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
-from django.contrib.auth import logout
+from django.contrib.auth import logout,login,authenticate
 from django.urls import reverse
 from django.http import HttpResponseRedirect
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 def logout_view(request):
     logout(request)
     return HttpResponseRedirect(reverse('learning_log:home'))
+def register(request):
+    if request.method != 'POST':
+        form = UserCreationForm()
+    else:
+        form = UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+            new_user = form.save()
+            authenticated_user = authenticate(username= new_user.username, password=request.POSR['password1'])
+            login(request, authenticated_user)
+            return HttpResponseRedirect(reverse('learning_log:home'))
+    context = {'form' : form}
+    return render(request, 'users/register.html', context)
